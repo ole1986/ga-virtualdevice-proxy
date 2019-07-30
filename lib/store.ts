9aa4cb0379349b55e8de264461ffc1b93f4ca779
ole1986 <ole.k@web.de>
2019-07-30 20:21:27 +0200

    Support for light and switches in FHEM

--- lib/store.ts
+++ lib/store.ts
@@ -1,3 +1,5 @@
 export { CommandShutterFhemBecker } from './CommandShutterFhemBecker';
 export { CommandShutterBecker } from './CommandShutterBecker';
 export { CommandShutterFhemGeneric } from './CommandShutterFhemGeneric';
+export { CommandSwitchFhemGeneric } from './CommandSwitchFhemGeneric';
+export { CommandLightFhemGeneric } from './CommandLightFhemGeneric';
