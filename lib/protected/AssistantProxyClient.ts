ba17cf09746b50f167df183e32b995d5dda7d080
ole1986 <ole.k@web.de>
2019-10-16 20:36:29 +0200

    sending keep alives using ping

--- lib/protected/AssistantProxyClient.ts
+++ lib/protected/AssistantProxyClient.ts
@@ -2,28 +2,54 @@ import * as WebSocket from 'ws';
 import { CommandFactory } from './CommandFactory';
 import * as config from '../../config.json';
 
+function getTimestamp() {
+    return Math.floor(new Date().getTime() / 1000);
+}
+
 export class AssistantProxyClient extends WebSocket {
+    private _responseTimeout: number = 10; // abort connection when ping response took to long
+    private _keepAliveInterval: number = 60; // requesting KeepAlive every minute
+    private _timeoutTimer;
+    private _timer;
+
     constructor() {
         super(config.REMOTE_HOST);
 
-        this.on('connectFailed', function (error) {
-            console.log('Connect Error: ' + error.toString());
+        this.on('connectFailed', err => {
+            clearInterval(this._timer);
+            console.log('Connect Error: ' + err.toString());
         });
-
-        this.on('open', this.loginUser);
-        this.on('ping', () => { console.log('.'); });
-
-        this.on('error', function (err) {
+        this.on('error', err =>  {
+            clearInterval(this._timer);
             console.error(err);
         });
 
-        this.on('close', function () {
+        this.on('open', this.loginUser);
+        this.on('pong', () => this.KeepAlive());
+        this.on('close', () => {
+            clearInterval(this._timer);
             console.log('Connection closed');
         });
 
         this.on('message', (x) => this.parseMessage(x));
+        
+        this._timer = setInterval(() => this.pingServer(), this._keepAliveInterval * 1000);
 
         CommandFactory.Proxy = this;
+    }
+
+    public KeepAlive() {
+        clearTimeout(this._timeoutTimer);
+        console.log("KeepAlive");
+    }
+
+    private pingServer() {
+        this.ping();
+        this._timeoutTimer = setTimeout( () => { 
+            console.log("Connection timed out");
+            this.terminate();
+            process.exit();
+        }, this._responseTimeout * 1000);
     }
 
     private async parseMessage(data: WebSocket.Data) {
